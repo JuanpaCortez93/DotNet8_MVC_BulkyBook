4241429460cffcff8fc790068fa302e0f9e1100a
Juan Pablo Cortez Mosquera <juanpacortexz93@gmail.com>
2024-03-11 10:29:56 -0500

    Agregar archivos de proyecto.

---
+++ 001_BulkiBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using _001_BulkiBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _001_BulkiBookWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 001_BulkiBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _001_BulkiBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
